003b0847e8cf577f7ecd3e789c72a2cd3e6c1edf
takahashisei-rks <sei.takahashi@rakus-partners.co.jp>
2024-08-08 10:26:00 +0900

    ex02

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,9 +2,11 @@ package com.example.controller;
 
 import com.example.service.AdministratorService;
 import com.example.form.InsertAdministratorForm;
+import com.example.domain.Administrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,15 @@ public class AdministratorController {
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+        administratorService.insert(administrator);
+        return "redirect:/";
     }
 }
